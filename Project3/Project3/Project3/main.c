f14da41a70f3b780a84a13602e869b54d32d12d2
mrutherford2 <mattrutherford@marionsoftware.com>
2014-10-27 16:46:49 -0400

    Added Comments
    Added comments to project 3 as required by project specifications.

--- Project3/Project3/Project3/main.c
+++ Project3/Project3/Project3/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include "..\..\..\stdbool.h"
 #include <math.h>
 
+/*
+Authors: Matt Rutherford, Megan Molumby, Abubakr Hassan
+Course: COP2220
+Project #: 3
+Title: Simple Operations
+Dude Date: 11/2/2014
+Test
+
+Takes in command line arguments for prime, factorial, and a leapyear. 
+Converts the command line arguments from character form to integer.
+Checks the user input to make sure it is an integer and within a specified range.
+Displays the amount of prime numbers, the calculated factorial, and if the year is a leapyear or not to the user.  
+*/
 
 //Function Prototypes
 bool withinRange(int input, int minRange, int maxRange, char *name);
@@ -16,26 +29,35 @@ void displayReport(int prime, int factorial, int leapYear, bool isLeapYearResult
 
 int main(int argc, char *argv[])
 {
+	//Variables to store our original values and calculated result values
 	int prime = 0, factorial = 0, leapYear = 0, primeCount = 0, factorialValue = 0;
 	bool isLeapYearResult = false;
 
+	//Displays Name of Authors and the Project
 	printf("Matt Rutherford, Megan Molumby, Abubakr Hassan\n"); 
 	printf("Project 3 - Simple Operations\n"); 
 
+	//Checks to see if we received the correct amount of arguments from the command line
 	if (argc == 4)
 	{
+		//Converts our command line arguments from strings to integers 
 		prime = atoi(argv[1]); 
 		factorial = atoi(argv[2]); 
 		leapYear = atoi(argv[3]);
 
+		//Calls validateInput function to see if we have valid input from user
 		if (validateInput(&prime, &factorial, &leapYear)==true)
-		{
+		{	
+			//Performs operations on values input from command line
 			performOperations(&prime, &factorial, &leapYear, &isLeapYearResult, &factorialValue, &primeCount);
+			
+			//Displays the results of our operations 
 			displayReport(prime, factorial, leapYear, isLeapYearResult, factorialValue, primeCount);
 		}
 	}
 	else
 	{
+		//Notifies the user that we did not receive the correct amount of command line arguments
 		printf("Did not get the correct amount of arguments"); 
 	}
 	
@@ -43,21 +65,39 @@ int main(int argc, char *argv[])
 	return 0; 
 }
 
+/*
+Validates our input values for prime, factorial, and leapyear. Checks to see if we have an integer value for 
+each of the inputs and if the value is within the specified range for each input.
+If it is not an integer value it will display the appropriate error message for each input value.
+*pPrime - Pointer for the prime value
+*pFactorial - Pointer for the factorial value
+*pLeapYear - Pointer for the leapyear value
+Returns the status of our flag variable for valid input 
+*/
 bool validateInput(int *pPrime, int *pFactorial, int *pLeapYear)
 {
+	//Flag variable for valid input, default value is set to false until we do our checking
 	bool isValid = false;
+	
+	//Checks to see if prime value is an integer and greater than 0
 	if (*pPrime > 0)
 	{
+		//Checks to see if the prime value is within range
 		if (withinRange(*pPrime, 1, 10000, "Prime")==true)
 		{
+			//Checks to see if the favortial value is an integer and greater than 0
 			if (*pFactorial > 0)
 			{
+				//Checks to see if the factorial is within range
 				if (withinRange(*pFactorial, 1, 12, "Factorial")==true)
 				{
+					//Checks to see if the leapyear value is an integer and greater than 0
 					if (*pLeapYear > 0)
 					{
+						//Checks to see if the leapyear value is within range
 						if (withinRange(*pLeapYear,1,4000, "Leap Year")==true)
 						{
+							//All input is good, set flag variable to a true value
 							isValid = true; 
 						}
 					}
@@ -81,8 +121,19 @@ bool validateInput(int *pPrime, int *pFactorial, int *pLeapYear)
 	return isValid; 
 }
 
+/*
+Checks the range of the integer values received from the command line.
+If the value is not within range the user is prompted with an error message
+displaying the min and max range of the value as well as the name of the value.
+input - The input for checking if it is within range
+minRange - The minimum range for our input
+maxRange - The maximum range for our input
+*name - Pointer for the name of our input value 
+Returns the status of our flag variable 
+*/
 bool withinRange(int input, int minRange, int maxRange, char* name)
 {
+	//Flag variable for valid range, set to true by default until we check the range 
 	bool isValid = true;
 
 	if (input < minRange || input > maxRange)
@@ -94,12 +145,25 @@ bool withinRange(int input, int minRange, int maxRange, char* name)
 	return isValid; 
 }
 
+/*
+Counts the amount of prime numbers between 0 and up to the number we pass into the function.
+If a number is prime between 0 and the number we pass into the function the prime count is 
+incremented by 1.
+number - The value of the integer we pass into the function to check how many prime numbers are between 0 and this number
+Returns the amount of prime numbers between 0 and up to the number we pass into the function
+*/
 int countPrimes(int number)
 {
+	//Set our primecount to zero until we check how many prime numbers are between 0 and the value we pass into the function
 	int primeCount = 0; 
-	int i; 
+
+	//Initialization of our index for the for loop so other compilers don't complain :)  
+	int i;
+
+	//Starts at 0 and counts up to and including the number we pass into the function and increments our index
 	for (int i = 0; i <= number; i++)
 	{
+		//If the index value is prime increment our prime count 
 		if (isPrime(i))
 		{
 			primeCount++; 
@@ -108,24 +172,39 @@ int countPrimes(int number)
 	return primeCount; 
 }
 
+/*
+Checks to see if the number we passed into the function is in fact prime.
+prime - The value that we are checking to see if it is a prime number
+Return the status of our flag variable 
+*/
 bool isPrime(int prime)
 {
+	//Flag variable for our prime number, set to true until we check if the number is prime
 	bool isPrime = true;
-	int p;
 
+	//Initilization of our index for the for loop so other compilers don't complain :) 
+	int i;
+
+	//If the prime value is less than 2 our flag is set to false
 	if (prime < 2)
 	{
 		isPrime = false;
 	}
+	
+	//If the prime value is evenly divisible by 2 and not equal to 2 our flag is set to false
 	else if (prime % 2 == 0 && prime != 2)
 	{
 		isPrime = false;
 	}
+
+	//Otherwise we start counting from 3, check all odd index values that are less than or equal to the square root of our prime value
+	//and our flag variable is true, and increments our index by 2 
 	else
 	{
-		for (p = 3; p <= sqrt(prime) && isPrime; p += 2)
+		for (i = 3; i <= sqrt(prime) && isPrime; i += 2)
 		{
-			if (prime % p == 0)
+			//If our prime value is evenly divisble by 0 set flag to false
+			if (prime % i == 0)
 			{
 				isPrime = false;
 			}
@@ -134,14 +213,23 @@ bool isPrime(int prime)
 	return isPrime;
 }
 
+/*
+Recursively calculates the factorial value of the input value we pass into the function and returns the calculated factorial value 
+factorial - The input value that we are performing the factorial calculation on
+Returns the calculated factorial value of the original value that we passed into the function 
+*/
 int getFactorial(int factorial)
 {
+	//Variable to store our calcuated factorial value 
 	int calculatedFactorial;
 
+	//If the factorial value that we passed into the function is equal to 0, the calculated value is 1 
 	if (factorial == 0)
 	{
 		calculatedFactorial = 1;
 	}
+	//Otherwise we recursively calcuate the value of the factorial value passed into the function
+	//by multiplying our original value by having the function call itself and passing in our original value minus 1. 
 	else
 	{
 		calculatedFactorial = factorial * getFactorial(factorial - 1);
@@ -150,11 +238,25 @@ int getFactorial(int factorial)
 	return calculatedFactorial; 
 }
 
+/*
+Checks to see if the leapyear value that we passed into the function is in fact a leapyear. If it is we return a true value, otherwise we return false
+year - The value of the year that we are passing into the function to see if it is a leapyear. 
+Returns the boolean value of our leapyear check, returns false unless our condition is met 
+*/
 bool isLeapYear(int year)
 {
 	return year % 4 == 0 && year % 100 != 0 || year % 400 == 0; 
 }
 
+/*
+Calls the functions to perform our operations for prime, factorial, and leapyear
+*prime - The pointer to the value of our prime number value from the command line
+*factorial - The pointer to the value of our factorial value input from the command line
+*leapYear - The pointer to the value of our leapyear value input from the command line
+*isLeapYearResult - The pointer to the boolean value of our result value for leapyear
+*factorialValue - The pointer to the calculated factorial result value 
+*primeCount - The pointer to the prime count result value 
+*/
 void performOperations(int *prime, int *factorial, int *leapYear, bool *isLeapYearResult, int *factorialValue, int *primeCount)
 {
 	*isLeapYearResult = isLeapYear(*leapYear);
@@ -162,6 +264,15 @@ void performOperations(int *prime, int *factorial, int *leapYear, bool *isLeapYe
 	*primeCount = countPrimes(*prime);
 }
 
+/*
+Displays the header as well as the results from our operations that we perform on our input values for prime, factorial, and leapyear. 
+prime - The prime value that was originally input from the command line
+factorial - The factorial value that was originally input from the command line
+leapYear- The leapyear value that was originally input from the command line
+isLeapYearResult - The result value for whether or not the original leapyear value is or is not a leapyear 
+factorialValue - The result value for our original value that we calculated the factorial for
+primeCount - The amount of prime numbers that were between 0 and the original prime value that was input from the command line 
+*/
 void displayReport(int prime, int factorial, int leapYear, bool isLeapYearResult, int factorialValue, int primeCount)
 {
 	printf("\n%s\n", "Operation Results:");
@@ -169,10 +280,12 @@ void displayReport(int prime, int factorial, int leapYear, bool isLeapYearResult
 	printf("%s%d%s%d\n", "Exactly ", primeCount, " prime numbers exist between 0 and ", prime);
 	printf("%s%d%s%d\n", "The value of ", factorial, "! is ", factorialValue);
 
+	//If the leapYearResult is false display the appropriate result message 
 	if (isLeapYearResult == false)
 	{
 		printf("%s%d%s", "The year ", leapYear, " is not a leap year.\n");
 	}
+	//Otherwise if the leapYearResult is not false display the appropriate result message 
 	else
 	{
 		printf("%s%d%s", "The year ", leapYear, " is a leap year.\n"); 
